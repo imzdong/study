6f8a67c6610970bd5c4ce9af16ea1a1b2e2e3d70
dong <dong>
2020-05-04 13:12:55 +0800

    泛型数组新增修改删除

--- design/src/main/java/org/imzdong/study/data/structure/array/ArrayTest.java
+++ design/src/main/java/org/imzdong/study/data/structure/array/ArrayTest.java
@@ -11,9 +11,30 @@ import java.util.List;
 public class ArrayTest {
 
     public static void main(String[] args) {
-        int a = 10;
-        a = 10 + (10>>1);
-        System.out.println(a);
+        //intArray();
+        strArray();
+    }
+
+    private static void strArray() {
+        GenericArrayCustom<String> arrayCustom = new GenericArrayCustom<>(10);
+        for(int num=0;num<12;num++){
+            arrayCustom.add("dong:"+num);
+        }
+        System.out.println(arrayCustom.length());
+        System.out.println(arrayCustom);
+        System.out.println("--------------------------------");
+        arrayCustom.insert(9,"dong:"+234);
+        System.out.println(arrayCustom.length());
+        System.out.println(arrayCustom);
+        System.out.println("--------------------------------");
+        System.out.println(arrayCustom.get(10));
+        System.out.println("--------------------------------");
+        arrayCustom.delete(10);
+        System.out.println(arrayCustom.length());
+        System.out.println(arrayCustom);
+    }
+
+    private static void intArray() {
         ArrayCustom arrayCustom = new ArrayCustom(10);
         for(int num=0;num<12;num++){
             arrayCustom.add(num);
@@ -30,6 +51,5 @@ public class ArrayTest {
         arrayCustom.delete(10);
         System.out.println(arrayCustom.length());
         System.out.println(arrayCustom);
-
     }
 }
