eda4f558064b3ef8640ffbb1a520cf532dfe8eab
dong <dong>
2020-05-05 22:20:19 +0800

    泛型数组新增修改删除

--- design/src/main/java/org/imzdong/study/data/structure/link/LinkDemo.java
+++ design/src/main/java/org/imzdong/study/data/structure/link/LinkDemo.java
@@ -1,5 +1,8 @@
 package org.imzdong.study.data.structure.link;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @description:
  * @author: Winter
@@ -7,5 +10,19 @@ package org.imzdong.study.data.structure.link;
  */
 public class LinkDemo {
 
+    public static void main(String[] args) {
+        LinkListCustom linkListCustom = new LinkListCustom();
+        System.out.println(linkListCustom);
+        linkListCustom.addTail(1);
+        linkListCustom.addTail(2);
+        linkListCustom.addTail(3);
+        linkListCustom.addTail(4);
+        System.out.println(linkListCustom);
+        System.out.println("-----------");
+        System.out.println(linkListCustom.get(2));
+
+        /*List<String> list = new LinkedList<>();
+        list.get(1);*/
+    }
 
 }
