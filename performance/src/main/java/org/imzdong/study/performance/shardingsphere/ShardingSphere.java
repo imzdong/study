9dae07468a428a3d2f67ceb4e2b96f0eaff0b337
dong <dong>
2020-07-18 15:52:57 +0800

    ShardingSphere原生java使用完善

--- performance/src/main/java/org/imzdong/study/performance/shardingsphere/ShardingSphere.java
+++ performance/src/main/java/org/imzdong/study/performance/shardingsphere/ShardingSphere.java
@@ -27,19 +27,21 @@ public class ShardingSphere {
         //创建分片规则配置类
         ShardingRuleConfiguration shardingRuleConfig = new ShardingRuleConfiguration();
         //创建分表规则配置类
-        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration("user", "ds${0..1}.user${0..1}");
+        TableRuleConfiguration tableRuleConfig = new TableRuleConfiguration("user", "shard${01..02}.user${0..1}");
+        //shard${01..02}.user${0..1} 表示：shard01: user0，user1; shard02：user0，user1；
+        //${begin..end} 表示的是一个从"begin"到"end"的范围区间，而多个 ${expression} 之间可以用"."符号进行连接，代表多个表达式数值之间的一种笛卡尔积关系。
         //创建分布式主键生成配置类
         Properties properties = new Properties();
         properties.setProperty("worker.id", "33");
         KeyGeneratorConfiguration keyGeneratorConfig = new KeyGeneratorConfiguration("SNOWFLAKE", "id", properties);
-        //result.setKeyGeneratorConfig(keyGeneratorConfig);
+        shardingRuleConfig.setDefaultKeyGeneratorConfig(keyGeneratorConfig);
         shardingRuleConfig.getTableRuleConfigs().add(tableRuleConfig);
         //根据性别分库，一共分为 2 个库
         shardingRuleConfig.setDefaultDatabaseShardingStrategyConfig(new InlineShardingStrategyConfiguration("sex", "ds${sex % 2}"));
         //根据用户 ID 分表，一共分为 2 张表
         shardingRuleConfig.setDefaultTableShardingStrategyConfig(new StandardShardingStrategyConfiguration("id", "user${id % 2}"));
         //final Map<String, DataSource> dataSourceMap, final ShardingRuleConfiguration shardingRuleConfig, final Properties props
-        DataSource realDataSource = ShardingDataSourceFactory.createDataSource(getMultipleDataSource(), shardingRuleConfig, properties);
+        DataSource realDataSource = ShardingDataSourceFactory.createDataSource(getMultipleDataSource(), shardingRuleConfig, new Properties());
         Connection connection = realDataSource.getConnection();
         String sql = "select * from user;";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
