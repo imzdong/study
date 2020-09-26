84d6cadd8431d9278981ac6749dd5f66d7ab66ca
dong <dong>
2020-09-26 14:29:56 +0800

    Bio服务端、客户端支持读写

--- msb_study/src/main/java/org/imzdong/study/msb/day_03_jdk_bio/ServerSocketDemo.java
+++ msb_study/src/main/java/org/imzdong/study/msb/day_03_jdk_bio/ServerSocketDemo.java
@@ -10,21 +10,32 @@ public class ServerSocketDemo {
     public static void main(String[] args) throws Exception{
         ServerSocket serverSocket = new ServerSocket(9090);
         while (true) {
-            Socket accept = serverSocket.accept();
+            Socket server = serverSocket.accept();
             new Thread(()->{
                 try {
-                    InputStream inputStream = accept.getInputStream();
+                    String name = Thread.currentThread().getName();
+                    System.out.println(name+ ":" + server.getPort()+":建立连接");
+                    //接受输入
+                    InputStream inputStream = server.getInputStream();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                    while (true){
-                        String s = reader.readLine();
-                        if(s != null) {
-                            System.out.println(accept.getPort()+":"+s);
-                        }
-                        if(Objects.equals(s,"close")){
-                            break;
+                    while (true) {
+                        if (reader.ready()) {
+                            String s = reader.readLine();//阻塞！！！！！
+                            if (s != null) {
+                                System.out.println(server.getPort() + ":" + s);
+                                OutputStream outputStream = server.getOutputStream();
+                                outputStream.write(("server res:"+s+"\r").getBytes());
+                                outputStream.flush();
+                                if (s.startsWith("close")) {
+                                    System.out.println(name+ ":" + server.getPort()+":连接close");
+                                    inputStream.close();
+                                    outputStream.close();
+                                    break;
+                                }
+                            }
                         }
                     }
-                    accept.close();
+                    server.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- msb_study/src/main/java/org/imzdong/study/msb/day_03_jdk_bio/SocketClientDemo.java
+++ msb_study/src/main/java/org/imzdong/study/msb/day_03_jdk_bio/SocketClientDemo.java
@@ -1,7 +1,6 @@
 package org.imzdong.study.msb.day_03_jdk_bio;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Objects;
 
@@ -9,19 +8,41 @@ public class SocketClientDemo {
 
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("127.0.0.1",9090);
+        String name = Thread.currentThread().getName();
+        System.out.println(name+"发起请求");
+        InputStream in = System.in;
+        while (true) {
+            System.out.println(name+"疯狂写入");
+            clientRead(socket);
+            if(clientWrite(socket, name, in)){
+                break;
+            };
+        }
+    }
+
+    private static boolean clientWrite(Socket socket,String name, InputStream in) throws IOException {
         OutputStream outputStream = socket.getOutputStream();
-        boolean flag = true;
-        while (flag) {
-            InputStream in = System.in;
-            byte[] bytes = new byte[1024];
-            int br = in.read(bytes);
-            String input = new String(bytes,0,br-1);
-            System.out.println(input);
-            if(Objects.equals(input,"close")){
-                flag = false;
-                outputStream.write(bytes);
-            }
-            outputStream.write(bytes);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String input = reader.readLine();//阻塞
+        System.out.println(name+":"+input);
+        input+="\r";
+        if(input.startsWith("close")){
+            System.out.println("client close");
+            outputStream.write(input.getBytes());
+            outputStream.close();
+            socket.close();
+            return true;
+        }
+        outputStream.write(input.getBytes());
+        outputStream.flush();
+        return false;
+    }
+
+    private static void clientRead(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        BufferedReader serverRead = new BufferedReader(new InputStreamReader(inputStream));
+        if(serverRead.ready()){
+            System.out.println("client read:"+serverRead.readLine());//阻塞
         }
     }
 }
