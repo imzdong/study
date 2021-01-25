c8daed0709d06f63149dc242c72694129eaaa22a
Imzdong <zhoudong_dream@163.com>
2021-01-25 08:23:54 +0800

    自定义标签

---
+++ spring/src/main/java/org/imzdong/study/spring/self/el/SelfUserBeanDefinitionParser.java
@@ -0,0 +1,26 @@
+package org.imzdong.study.spring.self.el;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
+import org.springframework.util.StringUtils;
+import org.w3c.dom.Element;
+
+public class SelfUserBeanDefinitionParser extends AbstractSingleBeanDefinitionParser {
+
+    @Override
+    protected Class<?> getBeanClass(Element element) {
+        return SelfUser.class;
+    }
+
+    @Override
+    protected void doParse(Element element, BeanDefinitionBuilder builder) {
+        String name = element.getAttribute("name");
+        String password = element.getAttribute("password");
+        if(StringUtils.hasLength(name)){
+            builder.addPropertyValue("name" , name);
+        }
+        if(StringUtils.hasLength(password)){
+            builder.addPropertyValue("password" , password);
+        }
+    }
+}
