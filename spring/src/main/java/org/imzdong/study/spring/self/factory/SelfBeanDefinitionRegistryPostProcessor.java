483afe9f46937aad58211e24060bd78974959545
Imzdong <zhoudong_dream@163.com>
2021-01-30 21:16:03 +0800

    自定义BeanDefinitionRegistryPostProcessor

---
+++ spring/src/main/java/org/imzdong/study/spring/self/factory/SelfBeanDefinitionRegistryPostProcessor.java
@@ -0,0 +1,18 @@
+package org.imzdong.study.spring.self.factory;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+public class SelfBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        System.out.println("SelfBeanDefinitionRegistryPostProcessor : postProcessBeanDefinitionRegistry");
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("SelfBeanDefinitionRegistryPostProcessor : postProcessBeanFactory");
+    }
+}
