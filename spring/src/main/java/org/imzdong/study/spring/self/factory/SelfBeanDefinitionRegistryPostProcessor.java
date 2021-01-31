9ad369745765c715c7e82ae7d9af98061f7bdbe7
Imzdong <zhoudong_dream@163.com>
2021-01-31 10:49:41 +0800

    ConfigurationClassPostProcessor

--- spring/src/main/java/org/imzdong/study/spring/self/factory/SelfBeanDefinitionRegistryPostProcessor.java
+++ spring/src/main/java/org/imzdong/study/spring/self/factory/SelfBeanDefinitionRegistryPostProcessor.java
@@ -4,7 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SelfBeanDefinitionRegistryPostProcessor implements BeanDefinitionRegistryPostProcessor {
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
