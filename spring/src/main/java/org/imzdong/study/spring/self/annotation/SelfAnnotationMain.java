beff96e44578b839f0f6bee5c2b6496eb992d071
Imzdong <zhoudong_dream@163.com>
2021-02-09 21:28:40 +0800

    Spring整体流程整理初版0.1

--- spring/src/main/java/org/imzdong/study/spring/self/annotation/SelfAnnotationMain.java
+++ spring/src/main/java/org/imzdong/study/spring/self/annotation/SelfAnnotationMain.java
@@ -1,13 +1,23 @@
 package org.imzdong.study.spring.self.annotation;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SelfAnnotationMain {
 
+    /**
+     * 先实例化及初始化所有的Bean
+     * @param args
+     */
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("self-annotation.xml");
         Person person = context.getBean(Person.class);
         System.out.println(person);
+        /*String path ="org/imzdong/study/spring/self/annotation";
+        ApplicationContext context = new AnnotationConfigApplicationContext(path);*/
+
+        GoodMan goodMan = context.getBean(GoodMan.class);
+        System.out.println(goodMan);
     }
 }
