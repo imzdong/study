3abeb95ceebf1d9088dbd24e8d4df357569ebf95
Imzdong <zhoudong_dream@163.com>
2021-03-04 09:41:18 +0800

    极客时间获取课程的文章列表

--- tool/src/main/java/org/imzdong/tool/geektime/GeekTimeArticles.java
+++ tool/src/main/java/org/imzdong/tool/geektime/GeekTimeArticles.java
@@ -1,9 +1,11 @@
 package org.imzdong.tool.geektime;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.imzdong.tool.util.OkHttpUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,38 +14,36 @@ import java.util.Map;
  */
 public class GeekTimeArticles {
 
-    private String columnId;
-    private String order;
+    private String cid;
     private String cookie;
-    private int size;
-    private int prev;
 
-    public GeekTimeArticles(String columnId, int size, int prev, String order, String cookie){
-        this.columnId = columnId;
-        this.order = order;
+    public GeekTimeArticles(String cid, String cookie){
+        this.cid = cid;
         this.cookie = cookie;
-        this.size = size;
-        this.prev = prev;
     }
 
-    public List<String> getArticleList(){
+    public List<GeekTimeArticle> getArticleList(){
         String url = GeekTimeConstant.articlesUrl;
         Map<String, String> headerMap = GeekTimeConstant.headers;
         headerMap.put(GeekTimeConstant.cookie,cookie);
-        String resp = null;
         try {
-            JSONObject bodyJson = new JSONObject();
-            bodyJson.put("cid", columnId);
-            bodyJson.put("size", size);
-            bodyJson.put("prev", prev);
-            bodyJson.put("order", order);
-            resp = OkHttpUtils.http(url, OkHttpUtils.getHeaders(headerMap),
-                    OkHttpUtils.getRequestBody(OkHttpUtils.TEXT, bodyJson.toJSONString()));
-            System.out.println(resp);
+            String articles = "{\"cid\":100006601,\"size\":500,\"prev\":0,\"order\":\"earliest\",\"sample\":false," +
+                    "\"chapter_ids\":[\"359\",\"360\",\"361\",\"362\",\"363\",\"364\",\"365\",\"366\",\"1310\"]}";
+            JSONObject bodyJson = JSONObject.parseObject(articles);
+            bodyJson.put("cid", cid);
+            String resp = OkHttpUtils.http(url, OkHttpUtils.getHeaders(headerMap),
+                    OkHttpUtils.getRequestBody(OkHttpUtils.JSON, bodyJson.toJSONString()));
             JSONObject result = JSONObject.parseObject(resp);
             if(result.containsKey("code")&&"0".equals(result.getString("code"))){
                 JSONObject data = result.getJSONObject("data");
-            }else {
+                JSONArray list = data.getJSONArray("list");
+                List<GeekTimeArticle> geekTimeArticles = new ArrayList<>(list.size());
+                list.forEach(m->{
+                    JSONObject articleJson = (JSONObject) m;
+                    GeekTimeArticle timeArticle = new GeekTimeArticle(articleJson.getString("id"), cookie);
+                    geekTimeArticles.add(timeArticle);
+                });
+                return geekTimeArticles;
             }
         } catch (IOException e) {
             e.printStackTrace();
