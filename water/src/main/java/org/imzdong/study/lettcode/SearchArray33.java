7d24dee7b1bd4b8671f68b2ed538e7a8ef3c8333
admin <imzdong>
2021-04-08 13:51:29 +0800

    解题思路，不过没想明白

--- water/src/main/java/org/imzdong/study/lettcode/SearchArray33.java
+++ water/src/main/java/org/imzdong/study/lettcode/SearchArray33.java
@@ -33,9 +33,95 @@ public class SearchArray33 {
 
     }
 
+    /**
+     * 二分查找(搜索旋转排序数组)
+     * k旋转，0-k-1升序，k-length升序 k-1最大值
+     * 如果target在0-k-1 或者k-1～length 二分查找
+     * mid的数据可能处于不同位置，处理方法不同
+     * target在0～k-1区间：直接二分查找
+     * target在k～length区间：直接二分查找
+     * mid的数和target比较
+     * 1、nums[mid] < target
+     *
+     * 简要来说：
+     *
+     * nums[0] <= nums[mid]（0 - mid不包含旋转）且nums[0] <= target <= nums[mid] 时 high 向前规约；
+     * nums[mid] < nums[0]（0 - mid包含旋转），target <= nums[mid] < nums[0] 时向前规约（target 在旋转位置到 mid 之间）
+     * nums[mid] < nums[0]，nums[mid] < nums[0] <= target 时向前规约（target 在 0 到旋转位置之间）
+     * 其他情况向后规约
+     * 也就是说nums[mid] < nums[0]，nums[0] > target，target > nums[mid] 三项均为真或者只有一项为真时向后规约。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     private static int search(int [] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+        while (left < right){
+            mid = (right - left)/2;
+            int midV = nums[mid];
+            if(midV == target){
+                return mid;
+            }
+            if(target > midV){
+                //left移动
+                if(midV <= nums[right]){
+                    left = mid + 1;
+                }else {
 
-        return 1;
+                }
+            }
+        }
+        return -1;
     }
 
 }
+/**
+ * 以二分搜索为基本思路
+ *
+ * 简要来说：
+ *
+ * nums[0] <= nums[mid]（0 - mid不包含旋转）且nums[0] <= target <= nums[mid] 时 high 向前规约；
+ * nums[mid] < nums[0]（0 - mid包含旋转），target <= nums[mid] < nums[0] 时向前规约（target 在旋转位置到 mid 之间）
+ * nums[mid] < nums[0]，nums[mid] < nums[0] <= target 时向前规约（target 在 0 到旋转位置之间）
+ * 其他情况向后规约
+ * 也就是说nums[mid] < nums[0]，nums[0] > target，target > nums[mid] 三项均为真或者只有一项为真时向后规约。
+ *
+ * 原文的分析是：
+ *
+ * 注意到原数组为有限制的有序数组（除了在某个点会突然下降外均为升序数组）
+ *
+ * if nums[0] <= nums[I] 那么 nums[0] 到 nums[i] 为有序数组,那么当 nums[0] <= target <= nums[i] 时我们应该在
+ * 0−i
+ * 0−i 范围内查找；
+ * if nums[i] < nums[0] 那么在
+ * 0
+ * −
+ * i
+ * 0−i 区间的某个点处发生了下降（旋转），那么
+ * I
+ * +
+ * 1
+ * I+1 到最后一个数字的区间为有序数组，并且所有的数字都是小于 nums[0] 且大于 nums[i]，当target不属于 nums[0] 到 nums[i] 时（target <= nums[i] < nums[0] or nums[i] < nums[0] <= target），我们应该在
+ * 0 − i
+ * 0−i 区间内查找。
+ * 上述三种情况可以总结如下：
+ *
+ *     nums[0] <= target <= nums[i]
+ *                target <= nums[i] < nums[0]
+ *                          nums[i] < nums[0] <= target
+ * 所以我们进行三项判断：
+ *
+ * (nums[0] <= target)， (target <= nums[i]) ，(nums[i] < nums[0])，现在我们想知道这三项中有哪两项为真（明显这三项不可能均为真或均为假（因为这三项可能已经包含了所有情况））
+ *
+ * 所以我们现在只需要区别出这三项中有两项为真还是只有一项为真。
+ *
+ * 使用 “异或” 操作可以轻松的得到上述结果（两项为真时异或结果为假，一项为真时异或结果为真，可以画真值表进行验证）
+ *
+ * 之后我们通过二分查找不断做小 target 可能位于的区间直到 low==high，此时如果 nums[low]==target 则找到了，如果不等则说明该数组里没有此项。
+ *
+ * 作者：LukeLee
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/ji-jian-solution-by-lukelee/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
