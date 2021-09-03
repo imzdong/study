ce3f2b7567ff601a2062de03c42e53bf36b12e7e
Winter <Winter>
2021-09-03 11:33:57 +0800

    选择排序

--- stone/src/main/java/org/imzdong/study/stone/interview/day0903/CommonSort.java
+++ stone/src/main/java/org/imzdong/study/stone/interview/day0903/CommonSort.java
@@ -9,7 +9,26 @@ public class CommonSort {
     public static void main(String[] args) {
         int arrays[] = {3, 2, 5, 8, 10, 2, 13, 15,18};
         //bubbleSort(arrays);
-        insertSort(arrays);
+        //insertSort(arrays);
+        /**
+         * 选择排序算法的实现思路有点类似插入排序，
+         * 也分已排序区间和未排序区间。
+         * 但是选择排序每次会从未排序区间中找到最小的元素，
+         * 将其放到已排序区间的末尾。
+         */
+        for (int i = 0; i < arrays.length; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arrays.length; j++) {
+                if(arrays[j] < arrays[minIndex]){
+                    minIndex = j;
+                }
+            }
+            if(minIndex != i){
+                int tmp = arrays[i];
+                arrays[i] = arrays[minIndex];
+                arrays[minIndex] = tmp;
+            }
+        }
         for (int arr : arrays) {
             System.out.println(arr);
         }
