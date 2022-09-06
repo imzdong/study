aba54d3589dbe0a44b8e8fae1cfe9c8244160bb0
imzdong <zhoudong_dream@163.com>
2022-09-06 10:22:13 +0800

    go mysql

--- study-go/bookstore/go.mod
+++ study-go/bookstore/go.mod
@@ -1,3 +1,7 @@
 module bookstore
 
 go 1.18
+
+//go get github.com/go-sql-driver/mysql
+//_ "github.com/go-sql-driver/mysql"
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
--- study-go/bookstore/store/store.go
+++ study-go/bookstore/store/store.go
@@ -1,7 +1,53 @@
 package store
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type Store struct {
 	Name   string
 	Author string
 	Date   string
+}
+
+func GetConnection() {
+	open, err := sql.Open("mysql", "root:root@(127.0.0.1:3303)/go-db")
+	if err != nil {
+		fmt.Printf("connection error:%v\n", err)
+		return
+	}
+	q := "select * from store"
+	query, qErr := open.Query(q)
+	if qErr != nil {
+		fmt.Printf("query error:%v\n", qErr)
+		return
+	}
+	var id int
+	var n, a, d string
+	for query.Next() {
+		err := query.Scan(&id, &n, &a, &d)
+		fmt.Printf("query err%v id:%d, n:%s, a:%s, d:%s\n", err, id, n, a, d)
+	}
+	query.Close()
+	open.Close()
+}
+
+func InsertStore() {
+	fmt.Println("insert before----")
+	open, err := sql.Open("mysql", "root:root@(127.0.0.1:3303)/go-db")
+	r, err := open.Exec("insert into store(name, author, date)values(?, ?, ?)", "zd", "winter", "2022-09-06")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		return
+	}
+	fmt.Println("insert succ:", id)
+	open.Close()
+	fmt.Println("insert after----")
 }
